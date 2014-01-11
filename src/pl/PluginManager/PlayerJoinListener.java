1a4fb3076736d6247503c17544a135dca5ef928f
UssCompany <UssCompany@users.noreply.github.com>
2014-01-11 21:34:53 +0100

    Create PlayerJoinListener.java

---
+++ src/pl/PluginManager/PlayerJoinListener.java
@@ -0,0 +1,16 @@
+package pl.PluginManager;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public final class PlayerJoinListener implements Listener {
+
+    @EventHandler
+    public void info(PlayerJoinEvent event) {
+    
+        
+    
+    }
+
+}
