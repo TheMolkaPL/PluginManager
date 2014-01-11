8caedc9def93661ae089acdd723c55790a6d5c57
eX1337PL <checkxpl@gmail.com>
2014-01-11 22:16:37 +0100

    Update ManagerCommand.java

--- src/pl/PluginManager/ManagerCommand.java
+++ src/pl/PluginManager/ManagerCommand.java
@@ -5,21 +5,20 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class ManagerCommand implements CommandExecutor {
-
-	private Main plugin;
+	
+	public String prefix = "§c[§6PluginManager§c] ";
+	
+	Main plugin;
 	
 	public ManagerCommand(Main plugin) {
 		this.plugin = plugin;
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("pluginmanager")) {
-			if (args.length == 1) {
-				if (args[0].equalsIgnoreCase("help")) {
-					sender.sendMessage("Plugin's help (to do)");
-					return true;
-				}
-			return false;
+		if (cmd.getName().equalsIgnoreCase("pluginmanager")){
+			if (args.length == 0){
+				Player player = (Player)sender;
+				player.sendMessage(prefix+"§cCorrect usage: §6/pluginmanager <help|on|off> [plugin name]");
 			}
 		}
 		return false;
