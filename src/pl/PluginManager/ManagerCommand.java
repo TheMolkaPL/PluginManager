97467be5f6457bd73e4790b6b7b40dbdf276b62b
UssCompany <uss.stanislaw@gmail.com>
2014-01-12 07:29:27 +0100

    Upgraded to work with console

--- src/pl/PluginManager/ManagerCommand.java
+++ src/pl/PluginManager/ManagerCommand.java
@@ -9,7 +9,7 @@ import org.bukkit.plugin.Plugin;
 
 public class ManagerCommand implements CommandExecutor {
 	
-	public String prefix = "§6»§cPluginManager§6« ";
+	public String prefix = "��6����cPluginManager��6�� ";
 	
 	Main plugin;
 	
@@ -20,44 +20,64 @@ public class ManagerCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		if(cmd.getName().equalsIgnoreCase("pluginmanager")){
 			if(args.length == 0){
-				Player player = (Player)sender;
-				player.sendMessage("§6Correct usage: §c/pluginmanager <help|on|off> [plugin name]");
+				sender.sendMessage("��6Correct usage: ��c/pluginmanager <help|on|off> [plugin name]");
 			}
 			if(args.length == 1){
 				if(args[0].equalsIgnoreCase("help")){
-					Player player = (Player)sender;
-					player.sendMessage("                       §6» §cPluginManager §6- §cv"+this.plugin.getDescription().getVersion()+" §6«");
-					player.sendMessage(" §6/pluginmanager on [plugin name] §c- §6enabling specify plugin");
-					player.sendMessage(" §6/pluginmanager off [plugin name] §c- §6disabling specify plugin");
-					player.sendMessage("                       §6» §cPluginManager §6- §cv"+this.plugin.getDescription().getVersion()+" §6«");
+					sender.sendMessage("                       ��6�� ��cPluginManager ��6- ��cv"+this.plugin.getDescription().getVersion()+" ��6��");
+					sender.sendMessage(" ��6/pluginmanager on [plugin name] ��c- ��6enabling specify plugin");
+					sender.sendMessage(" ��6/pluginmanager off [plugin name] ��c- ��6disabling specify plugin");
+					sender.sendMessage("                       ��6�� ��cPluginManager ��6- ��cv"+this.plugin.getDescription().getVersion()+" ��6��");
 					return true;
 				}
 			}
 			if(args.length == 2){
-				if(args[0].equalsIgnoreCase("on")){
-					Player player = (Player)sender;
-					if(player.hasPermission("pluginmanager.pluginon") || player.hasPermission("pluginmanager.*")){
-						Plugin plugin = Bukkit.getPluginManager().getPlugin(args[1]);
-						if(!(plugin == null)){
-							player.sendMessage(prefix+"§6Succesfuly enabled plugin §c"+plugin+"§6!");
+				if(args[0].equalsIgnoreCase("on")) {
+					if (sender instanceof Player) {
+						Player playersender = (Player)sender;
+						if(playersender.hasPermission("pluginmanager.pluginon") || playersender.hasPermission("pluginmanager.*")){
+							Plugin plugin = Bukkit.getPluginManager().getPlugin(args[1]);
+							if (!(plugin == null)) {
+								playersender.sendMessage(prefix+"��6Succesfuly enabled plugin ��c"+plugin+"��6!");
+								this.plugin.getServer().getPluginManager().enablePlugin(plugin);
+								return true;
+							}else{
+								playersender.sendMessage(prefix+"��6This plugin doesn't exist!");
+								return true;
+							}
+						}
+					}else{
+						if (!(plugin == null)) {
+							sender.sendMessage(prefix+"��6Succesfuly enabled plugin ��c"+plugin+"��6!");
 							this.plugin.getServer().getPluginManager().enablePlugin(plugin);
 							return true;
 						}else{
-							player.sendMessage(prefix+"§6This plugin doesn't exist!");
+							sender.sendMessage(prefix+"��6This plugin doesn't exist!");
 							return true;
 						}
 					}
 				}
 				if(args[0].equalsIgnoreCase("off")){
-					Player player = (Player)sender;
-					if(player.hasPermission("pluginmanager.pluginff") || player.hasPermission("pluginmanager.*")){
-						Plugin plugin = Bukkit.getPluginManager().getPlugin(args[1]);
-						if(!(plugin == null)){
-							player.sendMessage(prefix+"§6Succesfuly disabled plugin §c"+plugin+"§6!");
+					if (sender instanceof Player) {
+						Player playersender = (Player)sender;
+						if(playersender.hasPermission("pluginmanager.pluginff") || playersender.hasPermission("pluginmanager.*")){
+							Plugin plugin = Bukkit.getPluginManager().getPlugin(args[1]);
+							if (!(plugin == null)) {
+								playersender.sendMessage(prefix+"��6Succesfuly disabled plugin ��c"+plugin+"��6!");
+								this.plugin.getServer().getPluginManager().disablePlugin(plugin);
+								return true;
+							}else{
+								playersender.sendMessage(prefix+"��6This plugin doesn't exist!");
+								return true;
+							}
+						}
+					}else{
+						if (!(plugin == null)) {
+							sender.sendMessage(prefix+"��6Succesfuly disabled plugin ��c"+plugin+"��6!");
 							this.plugin.getServer().getPluginManager().disablePlugin(plugin);
 							return true;
 						}else{
-							player.sendMessage(prefix+"§6This plugin doesn't exist!");
+							sender.sendMessage(prefix+"��6This plugin doesn't exist!");
 							return true;
 						}
 					}
