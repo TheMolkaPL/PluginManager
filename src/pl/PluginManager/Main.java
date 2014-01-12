37d25ff45d295aae818c5aefbfc0b8bcdde1e014
eX1337PL <checkxpl@gmail.com>
2014-01-12 09:56:08 +0100

    Update Main.java

--- src/pl/PluginManager/Main.java
+++ src/pl/PluginManager/Main.java
@@ -6,31 +6,19 @@ import org.bukkit.ChatColor;
 
 public class Main extends JavaPlugin{
 	
-	
-	public String eplg = "default";
-	public String dplg = "default";
 	public InfoListener infolistener;
 	public String prefix = ChatColor.RED+"[" + ChatColor.GOLD + "PluginManager" + ChatColor.RED + "] ";
 	
-	public void onEnable(){
-		this.saveDefaultConfig();
-		this.saveConfig();
-		String enabledplg = (String) this.getConfig().get("enabled-plugin", eplg);
-		String disabledplg = (String) this.getConfig().get("disabled-plugin", dplg);
+	public void onEnable(){;
 		this.infolistener = new InfoListener(this);
 		this.getServer().getPluginManager().registerEvents(new InfoListener(this), this);
 		this.getCommand("pluginmanager").setExecutor(new ManagerCommand(this));
 		PluginDescriptionFile pdf = this.getDescription();
 		System.out.println("Plugin "+pdf.getName()+" v"+pdf.getVersion()+" has been enabled!");
-		eplg = enabledplg;
-		dplg = disabledplg;
-		this.reloadConfig();
 	}
 	
 	public void onDisable(){
 		PluginDescriptionFile pdf = this.getDescription();
 		System.out.println("Plugin "+pdf.getName()+" v"+pdf.getVersion()+" has been disabled!");
-		this.saveConfig();
-		this.reloadConfig();
 	}
 }
